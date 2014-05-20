ea0d3b781c9b17da3ed0e0f074b3171dbc938bb3
Patrick Smith <murrain101@yahoo.com>
2014-05-20 14:21:09 -0700

    updated team page. added google analytics

--- src/js/site.js
+++ src/js/site.js
@@ -1,3 +1,11 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-44268213-1', 'bottlestonightapp.com');
+ga('send', 'pageview');
+
 $(".phone_change").on("mouseover",function() {
 	$("#screenshot").attr("src","img/"+$(this).attr("id")+".jpg");
 	$(".phone_change").each(function(){
@@ -13,6 +21,49 @@ $(".phone_change").on("click",function() {
 		$(this).addClass("inactive");
 	});
 	$(this).addClass("active");
+});
+
+$("body").on('click','#download_cover.closed #cover_button', function(event) {
+	event.preventDefault();
+	open_download_cover();
+});
+
+$("body").on('click','#download_cover.open #cover_button', function(event) {
+	event.preventDefault();
+	close_download_cover();
+});
+
+function open_download_cover() {
+	$("#download_cover").removeClass('closed');
+	$("#download_cover").addClass('open');
+  	$("#download_cover").animate({ "top": "-=56px" }, "slow" );
+}
+function close_download_cover(){
+	$("#download_cover").removeClass('open');
+	$("#download_cover").addClass('closed');
+	$("#download_cover").animate({ "top": "+=56px" }, "slow" );
+}
+
+
+$("#text_link").on('submit', function(event){
+	event.preventDefault();
+
+	$.ajaxSetup({
+	    url: "download/text",
+	    global: false,
+	    type: "POST"
+	});
+
+	var post = $.ajax({
+	    data: $(this).serialize(),
+	    statusCode: {
+			200: function(data) 
+			{
+				close_download_cover();
+				$("#cover_button").html("Text sent");
+			}
+		}
+	});
 });
 
 $("#merchant_interest").on('submit', function(event){
@@ -37,3 +88,28 @@ $("#merchant_interest").on('submit', function(event){
 		}
 	});
 });
+
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+function download_for_mobile() {
+	$("#download_cover.closed").removeClass("closed");
+}
